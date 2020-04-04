f171ce3efcd4ee7df099432e14acf4066f01550f
ysczyl <1239864102@qq.com>
2020-04-04 17:54:42 +0800

    登陆，农户信息，顾客信息，购物，农产品信息模块提交

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 $(function() {
 	// Waves初始化
-	Waves.displayEffect();
+	// Waves.displayEffect();
 	// 输入框获取焦点后出现下划线
 	$('.form-control').focus(function() {
 		$(this).parent().addClass('fg-toggled');
@@ -8,7 +8,7 @@ $(function() {
 		$(this).parent().removeClass('fg-toggled');
 	});
 });
-Checkbix.init();
+// Checkbix.init();
 $(function() {
 	// 点击登录按钮
 	$('#login-bt').click(function() {
@@ -23,32 +23,37 @@ $(function() {
 });
 // 登录
 function login() {
+	sessionStorage.setItem('urlBase', 'http://192.168.31.114:8089');
+	var urlBase = sessionStorage.getItem('urlBase');
 	$.ajax({
-		url: BASE_PATH + '/sso/login',
-		type: 'POST',
-		data: {
-			username: $('#username').val(),
-			password: $('#password').val(),
-			rememberMe: $('#rememberMe').is(':checked'),
-			backurl: BACK_URL
-		},
+		url: urlBase + "/common/login",
+		type: 'post',
+		contentType:'application/json;charset=utf-8',
+        dataType:'json',
+		data: JSON.stringify({
+			userName: $('#username').val(),
+			userPass: $('#password').val(),
+		}),
 		beforeSend: function() {
 
 		},
 		success: function(json){
-			if (json.code == 1) {
-				location.href = json.data;
-			} else {
-				alert(json.data);
-				if (10101 == json.code) {
-					$('#username').focus();
-				}
-				if (10102 == json.code) {
-					$('#password').focus();
-				}
-			}
+			// console.log(json)
+			sessionStorage.setItem('userId', json.data.userId);
+			location.href = "./index.html"
+			// if (json.code == 1) {
+			// 	sessionStorage.setItem('role', '1');
+			// 	location.href = json.data;
+			// } else if(json.code == 2) {
+			// 	sessionStorage.setItem('role', '2');
+			// }else {
+			// 	sessionStorage.setItem('role', '3');
+			// }
 		},
 		error: function(error){
+			// location.href = "./index.html"
+			// sessionStorage.setItem('userId', '1');
+			$.alert("登陆失败请重试")
 			console.log(error);
 		}
 	});
